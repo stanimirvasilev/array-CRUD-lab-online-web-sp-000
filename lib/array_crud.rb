b9cf5d3ccfa3def0863d9a7cd313a3ac62731bad
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-01-22 14:33:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,19 +9,19 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [ 1, 2, 3, 4 ]
+
   array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
-  array = [ 1, 2, 3, 4 ]
+
   array.unshift (element)
   
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow", 2, 3,  "arrays!" ]
+ 
   array.pop 
   
 end
