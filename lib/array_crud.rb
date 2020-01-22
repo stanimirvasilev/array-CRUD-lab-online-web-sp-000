beea33a18a5f7bae83688e2e6e12f3a8c262c8e8
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-01-22 14:21:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,12 +35,13 @@ end
 
 
 def retrieve_element_from_index(array, index_number)
-  array = [[index_number]]
+  array = [index_number]
   
   
 end
 
 def retrieve_first_element_from_array(array)
+  
   
 end
 
